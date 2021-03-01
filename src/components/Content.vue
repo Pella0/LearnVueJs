a76d316065e4f760e5e32b1917fb5d14699bda54
Pella0 <pellattiero.ju@gmail.com>
2021-03-01 17:18:36 +0100

    transfert du code d'apprentissage, code mort dedans pour garder une trace

---
+++ src/components/Content.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+    <p>
+        {{txt}}
+    </p>
+    </div>
+    <!-- <List :prenom="prenom.taille"/> -->
+    <List :prenom="prenom"/>
+</template>
+
+<script>
+
+import List from './List/List.vue'
+
+export default {
+    name:'Content',  
+    data(){
+        return {
+            txt: 'je suis le contenu',
+            /* prenom: {
+                taille: 188 
+            } */
+            /* prenom: ['Fraise', 'banane'] */
+            prenom: function(){
+                console.log('Hello depuis le parents')
+            }
+        }
+    },
+    components: {
+        List
+    },
+}
+</script>
+
+
+
+<style scoped >
+</style>
---
+++ src/components/List/List.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <ul class="liste">
+            <li v-for="(fruit, index) in fruits" :key="index">{{fruit}}</li>
+        </ul>
+        <p>{{prenom()}}</p>
+        <!-- <p>{{reversing()}}</p> -->
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'List',
+    data(){
+        return{
+            fruits: ['fraise', 'banane', 'peche']
+        }
+    },
+    /* props:['prenom'], */
+    props:{
+        prenom: {
+            type: Function,
+            required: true
+        }
+    },
+   /*  methods: {
+        reversing : function(){
+            return this.prenom.split('').reverse().join('')
+        }
+    } */
+}
+</script>
+
+<style scoped src="./List.css">
+
+</style>
