694acb9f394a14eaa44700cf5d462102bc5b002a
Pella0 <pellattiero.ju@gmail.com>
2021-03-02 12:14:22 +0100

    test passage props, données parents enfants

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -1,15 +1,20 @@
 <template>
     <div class="container nt-5">
-    <p>
+        <h1>{{title}}</h1>
+ <!--    <p>
         {{txt}}
-    </p>
+    </p> -->
     
     <!-- <List :prenom="prenom.taille"/> -->
-    <List :prenom="prenom"/>
+    <!-- <List :prenom="prenom"/> -->
+    <List :myArr="myArr" :txt="txt" v-on:changeTitle="changementTitle($event)"/>
+    <!-- <List :myArr="myArr" :txt="txt"/> -->
+
     </div>
 </template>
 
 <script>
+
 
 import List from './List/List.vue'
 
@@ -17,20 +22,33 @@ export default {
     name:'Content',  
     data(){
         return {
-            txt: 'je suis le contenu',
+           /*  txt: 'je suis le contenu', */
             /* prenom: {
                 taille: 188 
             } */
             /* prenom: ['Fraise', 'banane'] */
-            prenom: function(){
+            /* prenom: function(){
                 console.log('Hello depuis le parents')
-            }
+            } */
+            myArr: [
+                {titre: 'Inception', date: 2010},
+                {titre: 'Ace Ventura', date: 1994},
+                {titre: 'Kamoulox', date: 2005},
+            ],
+            txt:'Hello World',
+            title: 'Je suis le titre'
         }
     },
     components: {
         List
     },
-}
+    methods: {
+     changementTitle: function(nwTitle){
+            this.title = nwTitle;
+        } 
+    }
+} 
+
 </script>
 
 
--- src/components/List/List.vue
+++ src/components/List/List.vue
@@ -1,11 +1,25 @@
 <template>
     <div>
-        <ul class="liste">
-            <li v-for="(fruit, index) in fruits" :key="index">{{fruit}}</li>
+        <ul class="liste mt-2">
+            <!-- <li v-for="(fruit, index) in fruits" :key="index">{{fruit}}</li> -->
+            <li v-for="(movie, index) in myArr" :key="index">
+                <div class="card">
+                    <div class="card-body">
+                        Titre : {{movie.titre}}
+                        <br/>
+                        Date : {{movie.date}}
+                    </div>
+                </div>
+            </li>
         </ul>
-        <p>{{prenom()}}</p>
+        <p>{{txt}}</p>
+        <!-- <p>{{prenom()}}</p> -->
         <!-- <p>{{reversing()}}</p> -->
-    </div>
+        <!-- <div v-on:click="supprFilm" class="btn btn-danger mt-2" >Supprime le film</div>
+        <div @click="supprTxt" class="btn btn-danger mt-2 ml-2" >Supprime le txt</div>
+    -->
+        <div @click="changeTitle" class="btn btn-danger mt-2 ml-2" >Modifier le titre</div>
+    </div> 
 </template>
 
 <script>
@@ -13,21 +27,39 @@ export default {
     name: 'List',
     data(){
         return{
-            fruits: ['fraise', 'banane', 'peche']
+             fruits: ['fraise', 'banane', 'peche'] 
         }
     },
+
+    ///PROPS///
+
     /* props:['prenom'], */
-    props:{
+   /*  props:{
         prenom: {
             type: Function,
             required: true
         }
-    },
+    }, */
+    props:['myArr', 'txt'],
+
+    ///METHODS///
+
    /*  methods: {
         reversing : function(){
             return this.prenom.split('').reverse().join('')
         }
     } */
+    methods: {
+        /* supprFilm: function(){
+            this.myArr.pop()
+        },
+        supprTxt: function(){
+            this.txt ="";
+        } */
+        changeTitle: function(){
+            this.$emit('changeTitle', 'Mon nouveau titre')
+        }
+    }
 }
 </script>
 
