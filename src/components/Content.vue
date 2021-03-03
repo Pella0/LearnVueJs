d813eb771870285fa381c0f612981442b665356c
Pella0 <pellattiero.ju@gmail.com>
2021-03-03 10:34:23 +0100

    systeme d'onglet/toggle dans Content via les composants Texte

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -8,21 +8,31 @@
     <!-- <List :prenom="prenom.taille"/> -->
     <!-- <List :prenom="prenom"/> -->
     <!-- v-on:changeTitle="changementTitle($event) -->
-    <List :myArr="myArr" :txt="txt">
+    <!-- <List :myArr="myArr" :txt="txt">
         <p>Envoyez</p>
         <template v-slot:info>
             <h1>Le contenu du slot nommé</h1>
         </template>
-    </List>
+    </List> -->
     <!-- <List :myArr="myArr" :txt="txt"/> -->
-
+            <div class="btn btn-outline-primary mr-2" @click='toggleOng1'>Onglet 1</div>
+            <div class="btn btn-outline-primary" @click='toggleOng2'>Onglet 2</div>
+            
+            <div class="card mb-5" v-if="toggle1">
+                <Texte1 class="p-5"/>
+            </div>
+            <div class="card mb-5" v-if="toggle2">
+                <Texte2 class="p-5"/>
+            </div>
     </div>
 </template>
 
 <script>
 
 
-import List from './List/List.vue'
+import Texte1 from './Text/Texte1'
+import Texte2 from './Text/Texte2'
+/* import List from './List/List.vue' */
 
 export default {
     name:'Content',  
@@ -42,17 +52,28 @@ export default {
                 {titre: 'Kamoulox', date: 2005},
             ],
             txt:'Hello World',
-            title: 'Je suis le titre'
+            title: 'Je suis le titre',
+            toggle1: true,
+            toggle2: false
         }
     },
     components: {
-        List
+        /* List */
+        Texte2,
+        Texte1
     },
     methods: {
      /* changementTitle: function(nwTitle){
             this.title = nwTitle;
         }  */
-        
+        toggleOng1: function(){
+            this.toggle1 = true
+            this.toggle2= false
+        },
+        toggleOng2: function(){
+            this.toggle1 = false
+            this.toggle2= true
+        }     
     }
 } 
 
